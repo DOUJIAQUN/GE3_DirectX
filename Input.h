92a067dcfae58c40c6176365914839f538116f97
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2024-10-07 15:26:04 +0900

    02_01

--- Input.h
+++ Input.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <dinput.h>
+#include <Windows.h>
 
-// 入力
 class Input {
+
 public:
-	// 初期化
-	void Initialize();
-	// 更新
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
+
+
 	void Update();
+
 
 };
