17a596d1fd51853500bfc8dae5311df891efc07e
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2024-10-21 16:38:40 +0900

    03_05完了

--- WinApp.h
+++ WinApp.h
@@ -2,27 +2,27 @@
 #include <Windows.h>
 #include <cstdint>
 #include "externals/imgui/imgui.h"
+#include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
-class WinApp
-{
+class WinApp {
 private:
-	HWND hwnd = nullptr;
-
-	WNDCLASS wc{};
+    HWND hwnd = nullptr;
+    WNDCLASS wc{};
 
 public:
-	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+    static constexpr int32_t kClientWidth = 1280;
+    static constexpr int32_t kClientHeight = 720;
 
-	static const int32_t kClientWidth = 1280;
-	static const int32_t kClientHeight = 720;
-public:
-	//
-	void Initialize();
-	//更新
-	void Update();
-	//終了
-	void Finalize();
+    // 初始化窗口
+    void Initialize();
+    // 处理
+    bool ProcessMessage();
+    // 终止
+    void Finalize();
 
-	HWND GetHwnd() const { return hwnd; }
-	HINSTANCE GetHInstance()const { return wc.hInstance; }
+    HWND GetHwnd() const { return hwnd; }
+    HINSTANCE GetHInstance() const { return wc.hInstance; }
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 };
--- resources/shaders/Object3d.vs.hlsl
+++ resources/shaders/Object3d.vs.hlsl
@@ -1,24 +1,23 @@
-#include "object2d.hlsli"
-
-struct TransFormationMatrix
+#include"Object3d.hlsli"
+struct TransformationMatrix
 {
     float32_t4x4 WVP;
     float32_t4x4 World;
+
 };
-ConstantBuffer<TransFormationMatrix> gTransformationMatrix : register(b0);
+ConstantBuffer<TransformationMatrix> gTransformtionMatrix : register(b0);
 
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
-    float32_t2 texccord : TEXCOORD0;
+    float32_t2 texcoord : TEXCOORD0;
     float32_t3 normal : NORMAL0;
 };
-
-VertexShaderOutput main(VertexShaderInput input)
+    VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrix.WVP);
-    output.texcoord = input.texccord;
-    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+    output.position = mul(input.position, gTransformtionMatrix.WVP);
+    output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformtionMatrix.World));
     return output;
 }
