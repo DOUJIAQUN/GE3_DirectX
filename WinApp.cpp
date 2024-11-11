d33ff958e648a9ca1effd7a1d36f530a5bf7d242
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2024-11-11 15:10:27 +0900

    03_03完了

--- WinApp.cpp
+++ WinApp.cpp
@@ -2,12 +2,14 @@
 #include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_win32.cpp"
 
+
 LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
     // ImGui 関連のメッセージ処理
     if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
         return true;
     }
+
     // メッセージに応じてゲーム固有の処理を行う
     switch (msg) {
         // ウィンドウが破棄された場合
@@ -16,6 +18,7 @@ LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
         PostQuitMessage(0);
         return 0;
     }
+
     // 標準のメッセージ処理を行う
     return DefWindowProc(hwnd, msg, wparam, lparam);
 }
@@ -23,16 +26,20 @@ LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 void WinApp::Initialize()
 {
     HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
+
     WNDCLASS wc{};
     wc.lpfnWndProc = WindowProc;
     wc.lpszClassName = L"CG2WindowClass";
     wc.hInstance = GetModuleHandle(nullptr);
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
     RegisterClass(&wc);
-    const int32_t kClientWidth = 1280;
-    const int32_t kClientHeight = 720;
+
+
+
     RECT wrc = { 0, 0, kClientWidth, kClientHeight };
     AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
     HWND hwnd = CreateWindow(
         wc.lpszClassName,
         L"CG2",
@@ -46,7 +53,12 @@ void WinApp::Initialize()
         wc.hInstance,
         nullptr
     );
+
     ShowWindow(hwnd, SW_SHOW);
+
+
 }
 
 void WinApp::Update()
+{
+}
