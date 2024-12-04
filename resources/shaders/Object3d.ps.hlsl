ed45a8dd190ed9ef8bc07552eccd0cbe77e63447
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2024-12-04 15:32:39 +0900

    11

--- resources/shaders/Object3d.ps.hlsl
+++ resources/shaders/Object3d.ps.hlsl
@@ -1,4 +1,4 @@
-#include "object2d.hlsli"
+#include "Object3d.hlsli"
 
 struct Material
 {
@@ -6,31 +6,36 @@ struct Material
     int32_t enableLighting;
     float32_t4x4 uvTransform;
 };
-ConstantBuffer<Material> gMaterial : register(b0);
-Texture2D<float32_t4> gTexture : register(t0);
-SamplerState gSampler : register(s0);
-struct DirectionalLight
-{
-    float32_t4 color;
-    float32_t3 direction;
-    float intensity;
-};
-ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
-
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
+struct DirectionalLight
+{
+    float32_t4 color;//ライトの色
+    float32_t3 direction;//ライトの向き
+    float intensity;//輝度
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
 
-PixelShaderOutput main(VertexShaderOutput input) {
+PixelShaderOutput main(VertexShaderOutput input)
+{
     PixelShaderOutput output;
-    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
-    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
-	if (gMaterial.enableLighting != 0){
+    output.color = gMaterial.color;
+    float4 transformedUV = mul(float32_t4(input.texcoord,0.0f,1.0f), gMaterial.uvTransform);
+    float32_t4 textureColor = gTexture.Sample(gSampler,transformedUV.xy);
+   
+    if (gMaterial.enableLighting != 0)//Lightingする場合
+    {
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-    }else{
+    }
+    else//Lightingしない場合。前回と同じ演算
+    {
         output.color = gMaterial.color * textureColor;
     }
     return output;
--- resources/shaders/Object3d.vs.hlsl
+++ resources/shaders/Object3d.vs.hlsl
@@ -1,16 +1,16 @@
-#include "object2d.hlsli"
+#include "Object3d.hlsli"
 
-struct TransFormationMatrix
+struct TransformationMatrix
 {
     float32_t4x4 WVP;
-    float32_t4x4 World;
+	float32_t4x4 World;
 };
-ConstantBuffer<TransFormationMatrix> gTransformationMatrix : register(b0);
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
-    float32_t2 texccord : TEXCOORD0;
+    float32_t2 texcoord : TEXCOORD0;
     float32_t3 normal : NORMAL0;
 };
 
@@ -18,7 +18,9 @@ VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
-    output.texcoord = input.texccord;
+    output.texcoord = input.texcoord;
     output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
     return output;
+
 }
+
